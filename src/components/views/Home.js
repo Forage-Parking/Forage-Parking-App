734a77cf160713030c32148e369595850bb4ebb7
Kamie Short <kamie.short@gmail.com>
2022-04-11 12:09:09 -0700

    install datetimepicker

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      FORAGE PARKING <br></br>The best parking app that ever existed! change here
+      FORAGE PARKING <br></br>The best parking app that ever existed!
     </div>
   );
 }
