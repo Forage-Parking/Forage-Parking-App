e2e6a4e3b31c1a2c3654f319a72f4f84e4e8ada0
Bradley-Bird <Bradleybird2@icloud.com>
2022-04-11 18:11:37 -0700

    basic map pin format added to home useEffect, ready for mapping and adding pins from supabase data

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { fetchSpots } from '../services/fetch';
+import Map from '../components/Map';
 
 export default function Home() {
   const [spots, setSpots] = useState([]);
@@ -14,11 +15,16 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
-      FORAGE PARKING <br></br>The best parking app that ever existed!
-      {spots.map((spot) => (
-        <p key={spot.id}>{`${spot.id}, ${spot.details}`}</p>
-      ))}
-    </div>
+    <>
+      <div>
+        FORAGE PARKING <br></br>The best parking app that ever existed!
+        {spots.map((spot) => (
+          <p key={spot.id}>{`${spot.id}, ${spot.details}`}</p>
+        ))}
+      </div>
+      <div>
+        <Map />
+      </div>
+    </>
   );
 }
