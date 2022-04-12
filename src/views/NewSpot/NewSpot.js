97b5fd62158ee343d056906feff306be7146c8ee
Kamie Short <kamie.short@gmail.com>
2022-04-12 15:43:17 -0700

    added state for newForm

--- src/views/NewSpot/NewSpot.js
+++ src/views/NewSpot/NewSpot.js
@@ -4,17 +4,26 @@ import Upload from '../../components/Upload/Upload';
 
 export default function NewSpot() {
   const [avatarUrl, setAvatarUrl] = useState(null);
+  const [ownerId, setOwnerId] = useState('user');
+  const [lat, setLat] = useState('');
+  const [lng, setLng] = useState('');
+  const [size, setSize] = useState('');
+  const [details, setDetails] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [price, setPrice] = useState('');
 
-  const [avatar_url, setAvatar_Url] = useState(null);
+  // const [avatar_url, setAvatar_Url] = useState(null);
+
+  console.log(avatarUrl);
 
   return (
     <>
       <div>
-        <SpotForm />
+        <SpotForm {...{ setOwnerId, setLat, setLng, setSize, setDetails, setNickname, setPrice }} />
       </div>
       <div>
         <Upload
-          url={avatar_url}
+          url={avatarUrl}
           size={150}
           onUpload={(url) => {
             setAvatarUrl(url);
