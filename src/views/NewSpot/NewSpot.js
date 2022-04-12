d5b76dfe924b7e83451ba46133c3ad63300c0c30
Kamie Short <kamie.short@gmail.com>
2022-04-12 13:14:00 -0700

    upload showing in my bucket

--- src/views/NewSpot/NewSpot.js
+++ src/views/NewSpot/NewSpot.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SpotForm from '../../components/SpotForm/SpotForm';
 import Upload from '../../components/Upload/Upload';
 
 export default function NewSpot() {
+  const [avatarUrl, setAvatarUrl] = useState(null);
+  // const [uploading, setUploading] = useState(false);
+  // const [loading, setLoading] = useState(true);
+  // const [username, setUsername] = useState(null);
+  // const [website, setWebsite] = useState(null);
+  const [avatar_url, setAvatar_Url] = useState(null);
+
   return (
     <>
       <div>
         <SpotForm />
       </div>
       <div>
-        <Upload />
+        <Upload
+          url={avatar_url}
+          size={150}
+          onUpload={(url) => {
+            setAvatarUrl(url);
+            // updateProfile({ username, website, avatar_url: url });
+          }}
+        />
       </div>
     </>
   );
