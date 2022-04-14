0852fbc8b1d326e4a214cdc830d6e7b8f8177449
Bradley-Bird <Bradleybird2@icloud.com>
2022-04-14 12:27:37 -0700

    started styling on home. commiting to pull from profile

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Nav from './Nav';
 import { Header } from 'grommet';
+import { IoLeafOutline } from 'react-icons/io5';
+import styled from 'styled-components';
 
 function HeaderNav() {
   return (
-    <Header>
-      <Nav />
+    <Header background="linear-gradient(to right, #c9d6ff, #e2e2e2)" pad="small" justify="between">
+      <Logo>
+        For
+        <IoLeafOutline />
+        ge
+      </Logo>
+      <Nav direction="row" />
     </Header>
   );
 }
 
+const Logo = styled.span`
+  font-size: 1rem;
+  font-weight: 400;
+`;
 export default HeaderNav;
