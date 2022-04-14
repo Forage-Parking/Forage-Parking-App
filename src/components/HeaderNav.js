239f20cb59921dd0aa943067de54f6ef2cc7a48b
Bradley-Bird <Bradleybird2@icloud.com>
2022-04-14 16:35:10 -0700

    colors added

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -3,10 +3,20 @@ import Nav from './Nav';
 import { Header } from 'grommet';
 import { IoLeafOutline } from 'react-icons/io5';
 import styled from 'styled-components';
+import { logout } from '../services/auth';
+import { useBasicContext } from '../context/BasicContext';
 
 function HeaderNav() {
+  const { setCurrentUser } = useBasicContext();
+  const handleLogout = async () => {
+    await logout();
+    setCurrentUser('');
+    history.go(0);
+  };
+
   return (
     <>
+      <Logout onClick={handleLogout}>logout</Logout>
       <Logo>
         For
         <IoLeafOutline />
@@ -26,5 +36,21 @@ const Logo = styled.span`
     color: #81b29a;
     transform: scale(-1, 1) rotate(90deg) translate(4px, -10%);
   }
+`;
+const Logout = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.5rem;
+  margin-right: 2rem;
+  text-decoration: none;
+  background: linear-gradient(35deg, #494949, #313131);
+  color: white;
+  width: 6rem;
+  height: 2rem;
+  cursor: pointer;
+  transform: scale(0.8);
+  z-index: 1000;
 `;
 export default HeaderNav;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,13 +4,10 @@ import styled from 'styled-components';
 import { NavLink, useHistory } from 'react-router-dom';
 import { IoLeafOutline } from 'react-icons/io5';
 import { fetchProfileByUserId } from '../services/fetch';
-import { getUserId, logout } from '../services/auth';
-import { useBasicContext } from '../context/BasicContext';
+import { getUserId } from '../services/auth';
 
 function Nav() {
   const [profile, setProfile] = useState({});
-  const { setCurrentUser } = useBasicContext();
-  const history = useHistory();
   const user = getUserId();
   useEffect(() => {
     const fetchData = async () => {
@@ -20,29 +17,20 @@ function Nav() {
     fetchData();
   }, [user]);
 
-  const handleLogout = async () => {
-    await logout();
-    setCurrentUser('');
-    history.go(0);
-  };
-
   return (
-    <>
-      <Logout onClick={handleLogout}>logout</Logout>
-      <StyledList>
-        <StyledNavLink exact to="/">
-          <IoLeafOutline />
-          <h4>Home</h4>
-        </StyledNavLink>
+    <StyledList>
+      <StyledNavLink exact to="/">
+        <IoLeafOutline />
+        <h4>Home</h4>
+      </StyledNavLink>
 
-        <StyledNavLink to={`/profile/${profile.id}`}>
-          <h4>Profile</h4>
-        </StyledNavLink>
-        <StyledNavLink to="/spots/new">
-          <h4>New Spot</h4>
-        </StyledNavLink>
-      </StyledList>
-    </>
+      <StyledNavLink to={`/profile/${profile.id}`}>
+        <h4>Profile</h4>
+      </StyledNavLink>
+      <StyledNavLink to="/spots/new">
+        <h4>New Spot</h4>
+      </StyledNavLink>
+    </StyledList>
   );
 }
 export const StyledList = styled.div`
